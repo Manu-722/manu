9f60fe5d8f2db8c3e9324942cbb3dcaf72755548
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-06-24 13:55:02 +0300

    imported django requirements

--- manu/urls.py
+++ manu/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('my_tags.urls')),
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
